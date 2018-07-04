48cca134eb164e0cd79aedd3bd42d7ac8ea305c3
Slava Tyhonchuk <slavatyhonchuk@gmail.com>
2018-07-04 23:27:01 +0300

    radio

--- dist/assets/js/scripts.min.js
+++ dist/assets/js/scripts.min.js
@@ -14,6 +14,7 @@
             btcPay.initCheckbox();
             btcPay.initSupportInfo();
             btcPay.initBalances();
+            btcPay.initUploadPhoto();
         },
         initHeader: function name(params) {
             let main = $('main'),
@@ -55,21 +56,56 @@
                 $(this).on('click', function () {
                     $(this).toggleClass('checked');
 
-                    if ($(this).closest(parent).find(hiddenCheckbox).prop('checked')) {
-                        $(this).closest(parent).find(hiddenCheckbox).prop('checked', false);
-                    }
-                    else {
-                        $(this).closest(parent).find(hiddenCheckbox).prop('checked', true);
+                    if ($(this).hasClass('fakeRadio')) {
+                        $(this).closest(parent).siblings().find('.fakeCheckbox').removeClass('checked');
+                        if ($(this).closest(parent).find(hiddenCheckbox).prop('checked')) {
+                            $(this).closest(parent).find(hiddenCheckbox).prop('checked', false);
+                            $(this).closest(parent).removeClass('radioActive');
+                        }
+                        else {
+                            $(this).closest(parent).siblings().find(hiddenCheckbox).prop('checked', false);
+                            $(this).closest(parent).siblings().removeClass('radioActive');
+                            $(this).closest(parent).addClass('radioActive');
+                            $(this).closest(parent).find(hiddenCheckbox).prop('checked', true);
+                        }
+                    } else {
+                        if ($(this).closest(parent).find(hiddenCheckbox).prop('checked')) {
+                            $(this).closest(parent).find(hiddenCheckbox).prop('checked', false);
+                        }
+                        else {
+                            $(this).closest(parent).find(hiddenCheckbox).prop('checked', true);
+                        }
                     }
                 });
-                if ($(this).closest(parent).find(hiddenCheckbox).prop('checked')) {
-                    $(this).closest(parent).find(checkbox).addClass('checked');
-                }
-                else {
-                    $(this).closest(parent).find(checkbox).removeClass('checked');
-                }
-            });
 
+
+            });
+            function initLoadCheckboxes() {
+                checkbox.each(function () {
+                    if ($(this).hasClass('fakeRadio')) {
+                        if ($(this).closest(parent).find(hiddenCheckbox).prop('checked')) {
+                            $(this).closest(parent).find(checkbox).addClass('checked');
+                            $(this).closest(parent).addClass('radioActive');
+                            $(this).closest(parent).siblings().find('.fakeCheckbox').removeClass('checked');
+                            $(this).closest(parent).siblings().removeClass('radioActive');
+                            $(this).closest(parent).siblings().find(hiddenCheckbox).prop('checked', false);
+                        }
+                        else {
+                            $(this).closest(parent).find(checkbox).removeClass('checked');
+                            $(this).closest(parent).removeClass('radioActive');
+
+                        }
+                    } else {
+                        if ($(this).closest(parent).find(hiddenCheckbox).prop('checked')) {
+                            $(this).closest(parent).find(checkbox).addClass('checked');
+                        }
+                        else {
+                            $(this).closest(parent).find(checkbox).removeClass('checked');
+                        }
+                    }
+                });
+            }
+            initLoadCheckboxes();
         },
         initSupportInfo: function () {
             if (!$('.showInfo').length) return false;
@@ -94,12 +130,23 @@
 
             openWithdraw.each(function (e) {
                 $(this).on('click', function (e) {
+                    console.log(e);
                     if ($(this).closest(balanceItem).hasClass('withdraw_opened')) return false;
+                    console.log(e);
 
+                    e.preventDefault();
                     $(this).closest(balanceItem).addClass('withdraw_opened');
 
                     e.stopPropagation();
                 });
+            });
+        },
+        initUploadPhoto: function (e) {
+            if (!$('#uploadPhoto').length) return false;
+            let uploadPhoto = $('#uploadPhoto'),
+                hiddenInput = $('#upload');
+            uploadPhoto.on('click', function () {
+                document.querySelector('input#upload').click();
             });
         },
     });
